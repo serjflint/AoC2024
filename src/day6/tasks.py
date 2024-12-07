bc088522890f3a5cc7b884c90949c09e9e9b2cfb
serjflint <serjflint@yandex-team.ru>
2024-12-07 23:53:54 +0300

    fix (day6): drop numpy

--- src/day6/tasks.py
+++ src/day6/tasks.py
@@ -1,15 +1,14 @@
 import collections
 import typing as tp
 
-import numpy as np
 from tqdm import tqdm
 
 from src.common import utils
 
-MASKS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+MASKS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
 
-def gen_steps(arr: np.ndarray, pos: tuple[int, int]) -> tp.Iterator[tuple[int, int]]:
+def gen_steps(arr, pos: tuple[int, int]) -> tp.Iterator[tuple[int, int]]:
     x, y = pos
     max_x, max_y = len(arr[0]), len(arr)
     d = 0
@@ -18,38 +17,47 @@ def gen_steps(arr: np.ndarray, pos: tuple[int, int]) -> tp.Iterator[tuple[int, i
         new_x, new_y = x + dir_x, y + dir_y
         if not (0 <= new_x < max_x and 0 <= new_y < max_y):
             break
-        if arr[new_x, new_y] == "#":
+        if arr[new_y][new_x] == "#":
             d = (d + 1) % 4
             continue
         yield new_x, new_y
         x, y = new_x, new_y
 
 
-def print_arr(arr: np.ndarray) -> None:
+def print_arr(arr) -> None:
     print("\n".join(["".join(row) for row in arr]))
     print()
 
 
+def where(arr, val) -> tp.Iterator[tuple[int, int]]:
+    max_x, max_y = len(arr[0]), len(arr)
+    for x in range(max_x):
+        for y in range(max_y):
+            if arr[y][x] == val:
+                yield x, y
+
+
 def task1() -> int:
-    arr = np.array([list(line.strip()) for line in utils.read_lines()])
-    pos_x, pos_y = np.argwhere(arr == "^")[0]
-    arr[pos_x, pos_y] = "X"
+    arr = [list(line.strip()) for line in utils.read_lines()]
+    pos_x, pos_y = next(where(arr, "^"))
+
+    arr[pos_y][pos_x] = "X"
     for x, y in gen_steps(arr, (pos_x, pos_y)):
-        arr[x, y] = "X"
-    return len(np.argwhere(arr == "X"))
+        arr[y][x] = "X"
+    return len(list(where(arr, "X")))
 
 
 CYCLE_COUNTER = 4
 
 
 def task2(filename: str = 'input.txt') -> int:
-    arr = np.array([list(line.strip()) for line in utils.read_lines(filename)])
-    pos_x, pos_y = np.argwhere(arr == "^")[0]
-    max_x, max_y = len(arr[0]), len(arr)
+    arr = [list(line.strip()) for line in utils.read_lines()]
+    pos_x, pos_y = next(where(arr, "^"))
 
+    max_x, max_y = len(arr[0]), len(arr)
     choices = {(x, y) for x, y in gen_steps(arr, (pos_x, pos_y))}
     choices.discard((pos_x, pos_y))
-    blocks = {(x, y) for x, y in np.argwhere(arr == "#")}
+    blocks = {(x, y) for x, y in where(arr, "#")}
 
     res = 0
     for obstacle in tqdm(choices):
